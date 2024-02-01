618b094a68e05826b5f013f8d0454cd0f60bc91e
SimoneFilograna <simonefilogranaf12@gmail.com>
2024-02-01 17:09:27 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,110 @@
+import { useState, useEffect } from 'react'
+import Card from './Components/Card';
+import './App.css';
+
+function App() {
+  const [search, setSearch] = useState('');
+  const [weather, setWeather] = useState(null)
+  const [coords, setCoords] = useState(null)
+
+  const key = 'd21e37561851e85923a0ef1efa2bf9af';
+
+  // fetch and set coord if we need using openweatherGeolocation 3.0 with subs
+
+  // const coords = async () => {
+  //   const res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${search}&appid=${key}`);
+  //   const data = await res.json();
+  //   setLat(data[0].lat)
+  //   setLong(data[0].lon)
+  // }
+
+  // obtain current coords
+
+  const currentPosition = () => {
+    navigator.geolocation.getCurrentPosition(position => {
+      setCoords({
+        long: position.coords.longitude,
+        lat: position.coords.latitude
+      })
+    })
+  }
+
+  // starting api call with long and lat 
+
+  const fetchCurrentWeather = async () => {
+    const fetchWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.long}&appid=${key}&lang=it&units=metric`);
+    const data = await fetchWeather.json()
+    setWeather(data)
+  }
+
+  // obtain coords if navigator is accepted by user
+  useEffect(() => {
+    if (navigator.geolocation) {
+      currentPosition();
+    }
+  }, [])
+
+  // Call api in coords state is not null
+  useEffect(() => {
+    if (coords) {
+      fetchCurrentWeather()
+    }
+  }, [coords])
+
+
+  // fetch weather using openweather 2.5 with free usage
+  const fetchWeather = async () => {
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search},it&appid=${key}&lang=it&units=metric`)
+    const data = await res.json();
+    setWeather(data)
+  }
+
+  // setting search state from input text
+  const handleSearch = (e) => {
+    setSearch(e.target.value)
+  }
+
+  // send form with finish data and fetch weather 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetchWeather()
+    setSearch("")
+  }
+
+
+  return (
+    <main>
+
+      <div className="container">
+
+        <div className="title-container text-center mt-5">
+          <h1 className='display-1 fw-semibold fst-italic'>React Weather</h1>
+        </div>
+
+        <div className='mt-5'>
+          <form onSubmit={handleSubmit}>
+            <div className="d-flex gap-3 justify-content-center">
+
+              {/* setting controlled input */}
+              <input className='form-control w-25'
+                type="text"
+                placeholder='Search your city'
+                id='search'
+                name='search'
+                onChange={handleSearch}
+                value={search}
+              />
+              <button type='submit' className='btn btn-primary'>Search</button>
+            </div>
+          </form>
+
+        </div>
+
+        {weather && <Card {...weather} />}
+      </div>
+    </main>
+  )
+}
+
+
+export default App
---
+++ src/Components/Card.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Card = ({ coord, weather, main, wind, name }) => {
+    const capitalizedWeather = (weather[0].description).slice(0, 1).toUpperCase() + weather[0].description.slice(1);
+    console.log(capitalizedWeather)
+
+    return (
+        <section>
+            <div className="card mt-5">
+                <div className="card-body d-flex gap-5 align-items-center pt-3 pb-2 bg-light text-center">
+                    <div>
+                        <img src={`https://openweathermap.org/img/wn/${weather[0].icon}.png`} className='' alt="icon" />
+                    </div>
+                    <h3 className="card-title">{name}</h3>
+                    <p className="card-text">Longitudine: {coord.lon}</p>
+                    <p className="card-text">Latitudine: {coord.lat}</p>
+                    <p className="card-text">{capitalizedWeather}</p>
+                    <p>Max: {main.temp_max}</p>
+                    <p>Min: {main.temp_min}</p>
+                    <p>Umidità: {main.humidity}%</p>
+                    <p>Velocità vento: {wind.speed}Km/h</p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Card
